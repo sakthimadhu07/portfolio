641ddcbe8c3a630409075a3e62525db029ba2dd5
sakthi madhu <ersakthi72@gmail.com>
2023-12-09 06:17:08 +0530

    modified-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,22 +108,19 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
   });
 
 /*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', { 
+/*let swiperPortfolio = new Swiper('.portfolio__container', {
+    cssMode: true,
     loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
-      dynamicBullets: true,
     },
-    breakpoints:{
-        568:{
-            slidesPerView: 2,
-        }
-    }
-  });
+  });*/
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
